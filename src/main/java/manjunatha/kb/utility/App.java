2ec332878a6e690bece8343c7f4e7923ae44e1ad
manjunatha-kanekal <kanekal.manjunatha@gmail.com>
2018-02-28 16:18:52 +0530

    Modified to take the file paths from the command line arguments.

--- src/main/java/manjunatha/kb/utility/App.java
+++ src/main/java/manjunatha/kb/utility/App.java
@@ -7,10 +7,23 @@ import java.io.FileNotFoundException;
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        String folderPath = "D:\\workspace\\FolderName\\src";
+    public static void main( String[] args ) throws IllegalArgumentException {
+
+        String folderPath;
+        String inputExcelFilePath;
+        String outputExcelFilePath;
+
+        try{
+            folderPath = args[0];
+            inputExcelFilePath = args[1];
+            outputExcelFilePath = args[2];
+        } catch(Exception e) {
+            throw new IllegalArgumentException("Please pass the SearchFolder, InputExcel and OutputExcel file paths.");
+        }
+
+        /*String folderPath = "D:\\workspace\\FolderName\\src";
         String inputExcelFilePath = "C:\\Users\\Manjunath B K\\Desktop\\DependencyAnalyser\\Input.xlsx";
-        String outputExcelFilePath = "C:\\Users\\Manjunath B K\\Desktop\\DependencyAnalyser\\Output.xlsx";
+        String outputExcelFilePath = "C:\\Users\\Manjunath B K\\Desktop\\DependencyAnalyser\\Output.xlsx";*/
 
         try {
             DependencyAnalyser analyser = new DependencyAnalyser(folderPath, inputExcelFilePath, outputExcelFilePath);
